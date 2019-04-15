91020e61962ba8bba5f1bf2b3828841aaff38bb7
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-15 16:34:15 +0000

    Week7 Day1 Created

---
+++ Week7/Day1/practice.rb
@@ -0,0 +1,10 @@
+def sayHello(name)
+  puts "Hello, #{name}"
+end
+sayHello("Hao Jiang")
+puts(">>>>>>>>>>")
+myArray = [5, 4, 3, 2, 1]
+myArray.sort
+puts(myArray)
+puts(">>>>>>>>>>")
+myArray.each{|word| puts word}
