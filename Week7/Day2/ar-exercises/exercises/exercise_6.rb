6fdae5a07d08bd771257e44fae0ae8e383e4f3ff
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-16 20:37:35 +0000

    exercise_6.rb updated

--- Week7/Day2/ar-exercises/exercises/exercise_6.rb
+++ Week7/Day2/ar-exercises/exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Hao", last_name: "Jiang", hourly_rate: 88)
+@store2.employees.create(first_name: "Ni", last_name: "Peng", hourly_rate: 99)
