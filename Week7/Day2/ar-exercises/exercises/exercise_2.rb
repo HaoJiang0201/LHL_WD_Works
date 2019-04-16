40f6929225d81c8d4568b21238945a72c29dc6f7
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-16 22:25:10 +0000

    All Completed

--- Week7/Day2/ar-exercises/exercises/exercise_2.rb
+++ Week7/Day2/ar-exercises/exercises/exercise_2.rb
@@ -7,5 +7,5 @@ puts "----------"
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
-@store1.mens_apparel = false
+@store1.mens_apparel = true
 @store1.save
--- Week7/Day2/ar-exercises/exercises/exercise_7.rb
+++ Week7/Day2/ar-exercises/exercises/exercise_7.rb
@@ -10,3 +10,34 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# puts @store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60).errors.details[:hourly_rate]
+employeeNew = @store2.employees.create(hourly_rate: 30)
+if(!employeeNew.valid?)
+    puts
+    puts "Create Employee Error: "
+    employeeNew.errors.messages.each do |key, value|
+        puts "#{key}: #{value}"
+    end
+    puts
+end
+
+# Employee.new
+# @store2.employees = Employee.new
+# @store2.employees.first_name = "Khurram"
+# @store2.employees.last_name = "Virani"
+# @store2.employees.hourly_rate = 60
+# @store2.employees.save
+
+puts "Input a new store's name:"
+newName = gets.chomp
+
+storeNew = Store.create(name:newName, annual_revenue: -1, mens_apparel: false, womens_apparel: false)
+if(!storeNew.valid?)
+    puts
+    puts "Create Store Error: "
+    storeNew.errors.messages.each do |key, value|
+        puts "#{key}: #{value}"
+    end
+    puts
+end
