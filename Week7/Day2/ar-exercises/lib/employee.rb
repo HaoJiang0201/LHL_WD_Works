90c78562807f07c7772b3b6ee7a1ea5fe17a7709
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-16 22:28:56 +0000

    employee and store modified: validations added.

--- Week7/Day2/ar-exercises/lib/employee.rb
+++ Week7/Day2/ar-exercises/lib/employee.rb
@@ -1,3 +1,14 @@
 class Employee < ActiveRecord::Base
-
+    belongs_to :store
+    validates :first_name, presence: { message: "first_name cannot be empty" }
+    validates :last_name, presence: { message: "last_name cannot be empty" }
+    validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, lesss_than_or_equal_to: 200, message: "must between[40, 200]" }
+    # validate :hourly_rate_range
+    # def hourly_rate_range
+    #     if hourly_rate < 40
+    #         errors.add(:hourly_rate, "cannot be below 40")
+    #     elsif hourly_rate > 200
+    #         errors.add(:hourly_rate, "cannot be above 200")
+    #     end
+    # end
 end
--- Week7/Day2/ar-exercises/lib/store.rb
+++ Week7/Day2/ar-exercises/lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
-
+    has_many :employees
+    validates :name, presence: { message: "must has a name" }
+    validates :name, length: { minimum: 3, message: "no less than 3 characters" }
+    validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true, message: "must be a positive integer" }
+    validate :must_carry_mens_or_womens_apparel
+    def must_carry_mens_or_womens_apparel
+        if !mens_apparel && !womens_apparel
+            errors.add(:mens_apparel, "the store must be men's or women's apparel")
+            errors.add(:womens_apparel, "the store must be men's or women's apparel")
+        end 
+        # errors.add(:womens_apparel, "the store must be men's or women's apparel") unless womens_apparel
+    end
 end
