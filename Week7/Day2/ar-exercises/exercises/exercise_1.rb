5d6cd24a014c0e32a1af69ec2c25fd06ba573be8
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-16 19:24:11 +0000

    exercise_1.rb exercise_2.rb exercise_3.rb exercise_4.rb updated

--- Week7/Day2/ar-exercises/exercises/exercise_1.rb
+++ Week7/Day2/ar-exercises/exercises/exercise_1.rb
@@ -6,7 +6,6 @@ puts "----------"
 # Your code goes below here ...
 
 store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-puts "Burnaby added to the db..."
 
 store2 = Store.new
 store2.name = "Richmond"
@@ -14,9 +13,7 @@ store2.annual_revenue = 1260000
 store2.mens_apparel = false
 store2.womens_apparel = true
 store2.save
-puts "Richmond added to the db..."
 
 store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-puts "Gastown added to the db..."
 
 puts "Totally #{Store.count} stores in the db..."
--- Week7/Day2/ar-exercises/exercises/exercise_2.rb
+++ Week7/Day2/ar-exercises/exercises/exercise_2.rb
@@ -6,10 +6,6 @@ puts "----------"
 
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
-puts "store1: #{@store1.name}"
 @store2 = Store.find_by(id: 2)
-puts "store2: #{@store2.name}"
-
-@store1.name = "Surey"
+@store1.mens_apparel = false
 @store1.save
-puts "store1: #{@store1.name}"
