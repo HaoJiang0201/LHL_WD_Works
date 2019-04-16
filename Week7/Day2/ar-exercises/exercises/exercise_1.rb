98fe59bd7c5e40fdc50e781f800173a7df77b3b9
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-16 17:52:52 +0000

    excersice 1 completed

--- Week7/Day2/ar-exercises/exercises/exercise_1.rb
+++ Week7/Day2/ar-exercises/exercises/exercise_1.rb
@@ -4,3 +4,18 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+end
+
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+puts "Burnaby added to the db..."
+store2 = Store.new
+store2.name = "Richmond"
+store2.annual_revenue = 1260000
+store2.mens_apparel = false
+store2.womens_apparel = true
+store2.save
+puts "Richmond added to the db..."
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts "Gastown added to the db..."
